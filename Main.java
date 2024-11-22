eb0c3c48b86f7bd990f492ca04613e344c3cbdc8
“Nastya” <nastya.cap04@gmail.com>
2024-11-22 14:24:12 +0300

    Add calculated amounts

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Main{
     public static void main(String[] args) {
         String text = "Это новый текст для задания 2";
@@ -5,5 +7,16 @@ class Main{
             System.out.println(word);
         }
         System.out.print("Это добавлено в ветке dev");
+
+
+        System.out.println("Теперь опять проверим работу суммирования");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        int a = scanner.nextInt();
+        System.out.println("Введите второе число");
+        int b = scanner.nextInt();
+        int res = a + b;
+        System.out.println("Вот их сумма: " + res);
+
     }
 } 
