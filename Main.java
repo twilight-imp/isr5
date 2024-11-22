113d483dbf547984c616d13e4954331d19f16a6a
“Nastya” <nastya.cap04@gmail.com>
2024-11-22 14:22:52 +0300

    Add split text

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 class Main{
     public static void main(String[] args) {
+        String text = "Это новый текст для задания 2";
+        for (String word: text.split(" ")) {
+            System.out.println(word);
+        }
         System.out.print("Это добавлено в ветке dev");
     }
 } 
