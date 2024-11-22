d5048757082fcea78caee16af6601ea9a1adc576
“Nastya” <nastya.cap04@gmail.com>
2024-11-22 13:24:08 +0300

    Del all calculations and add time output

--- Main.java
+++ Main.java
@@ -1,24 +1,10 @@
-import java.util.Scanner;
+import java.time.LocalTime;
 
 class Main{
     public static void main(String[] args) {
 
-        System.out.println("Теперь проверим работу суммирования");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите первое число");
-        double a = scanner.nextDouble();
-        System.out.println("Введите второе число");
-        double b = scanner.nextDouble();
-        double res = a + b;
-        System.out.println("Вот их сумма: " + res);
-
-        System.out.println("Теперь проверим работу вычитания");
-        System.out.println("Введите первое число");
-        double c = scanner.nextDouble();
-        System.out.println("Введите второе число");
-        double d = scanner.nextDouble();
-        double res2 = a - b;
-        System.out.println("Вот их разность: " + res2);
+        System.out.println("Это код для последнего коммита в 1 задании");
+        System.out.println("Выведем точное время:" + LocalTime.now());
 
     }
 } 
