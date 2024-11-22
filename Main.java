1a01dfe7c016b6edc6cf8a963a2710c528c4631d
“Nastya” <nastya.cap04@gmail.com>
2024-11-22 15:00:14 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.time.LocalTime;
 class Main{
     public static void main(String[] args) {
 
-        System.out.println("Это код для последнего коммита в 1 задании");
+        System.out.println("Это код для коммита в 3 задании");
         System.out.println("Выведем точное время:" + LocalTime.now());
 
     }
