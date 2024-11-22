824da820dbb2afe0c9dc57b39673d026a871f8a2
“Nastya” <nastya.cap04@gmail.com>
2024-11-22 14:26:32 +0300

    Dell all calculations and add time output

--- Main.java
+++ Main.java
@@ -1,30 +1,11 @@
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 class Main{
     public static void main(String[] args) {
-        String text = "Это новый текст для задания 2";
-        for (String word: text.split(" ")) {
-            System.out.println(word);
-        }
-        System.out.print("Это добавлено в ветке dev");
 
-
-        System.out.println("Теперь опять проверим работу суммирования");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите первое число");
-        int a = scanner.nextInt();
-        System.out.println("Введите второе число");
-        int b = scanner.nextInt();
-        int res = a + b;
-        System.out.println("Вот их сумма: " + res);
-
-        System.out.println("Теперь опять проверим работу вычитания");
-        System.out.println("Введите первое число");
-        int c = scanner.nextInt();
-        System.out.println("Введите второе число");
-        int d = scanner.nextInt();
-        int res2 = c + d;
-        System.out.println("Вот их разность: " + res2);
+        System.out.println("Это код для последнего коммита во 2 задании");
+        System.out.println("Выведем точную дату: " + LocalDate.now());
 
     }
 } 
